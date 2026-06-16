c89ec7ba852d7c3fcef3b592e14521542b1e1f59
shiyj35891 <shiyj35891@hundsun.com>
2026-06-16 16:40:44 +0800

    删除文件

--- file_methods.py
+++ file_methods.py
@@ -1,8 +1,13 @@
 import time
 
-with open('test_txt.txt', 'w', buffering=2, encoding='utf-8') as file:
-  for i in range(5):
-    file.write(f'写入：{i}')
-    file.flush()
-    time.sleep(1)
-    file.write(f'({i})\n')
+def write_file():
+  with open('test_txt.txt', 'w', buffering=2, encoding='utf-8') as file:
+    for i in range(5):
+      file.write(f'写入：{i}')
+      file.flush()
+      time.sleep(1)
+      file.write(f'({i})\n')
+
+def read_file():
+  with open('')
+
